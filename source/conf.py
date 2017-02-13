37de9aa9004d64dea94a0c81582071bc992b0ae0
Nikolay Shuyskiy <klas@genestack.com>
2017-02-13 14:25:11 +0300

    Bump year in copyrights

--- source/conf.py
+++ source/conf.py
@@ -53,7 +53,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Genestack User Tutorials'
-copyright = u'2016, Genestack'
+copyright = u'2017, Genestack'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
